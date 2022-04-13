7d2080fb414a8b47de503f80173cc4f911fa3e36
Alexanderpmann <99506481+Alexanderpmann@users.noreply.github.com>
2022-04-13 06:38:05 -0400

    updating to proper server.py file
    Used resources from Great Numba Game, Learn Platform, and interwebs search for fully understanding code. 
    
    - once I realized the code wasn't the actual issue and the localhost was wrong -> major facepalm.

--- Counter/server.py
+++ Counter/server.py
@@ -1,11 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
+app.secret_key = 'Count_da_counting_da_Nums' #secret key is secrets
 
 
-@app.route('/')
+# refreshing page will increase site count
+@app.route("/")
 def index():
-    return render_template("index.html")
+    if "count" not in session:
+        session["count"] = 1
+    else:
+        session["count"] += 1 
+# defining the count = session for browser to pickup code
+    return render_template('index.html', count = session["count"])
+
+# Button in form content container will increment by 2
+@app.route("/count", methods = ["POST"])
+def view_count():
+    if (request.form["change"]) == "add":
+        session["count"] += 1
+    elif (request.form["change"]) == "reset":
+        session["count"] = 0
+    
+    return redirect('/')
+
+# Clearing session - Code copied from Great Numba Game
+@app.route("/reset")
+def reset_session():
+    session.clear()
+    return redirect("/")
 
 
-if __name__ == ('__main__'):
-    app.run (debug=True)
+#debug / remove if publishing/finished 
+if __name__ == "__main__":
+    app.run(debug=True)
